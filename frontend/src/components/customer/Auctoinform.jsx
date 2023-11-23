e97c5e9832e511520c5be4f8f3c1448d9a988ff6
wasikurosaki <washikur.rahman@g.bracu.ac.bd>
2023-11-23 12:32:17 +0600

    nothing

--- frontend/src/components/customer/Auctoinform.jsx
+++ frontend/src/components/customer/Auctoinform.jsx
@@ -48,7 +48,7 @@ const Auctoinform = () => {
   return (
     <div>
       <form className="w-full max-w-lg my-10" onSubmit={handleSubmit}>
-        <div className="flex flex-wrap -mx-3 mb-6 ">
+        <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="carName">
               Car Name
